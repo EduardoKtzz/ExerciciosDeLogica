2ea2d20c4d958d60ae5eb475690587f8f283462d
Eduardo Klitzke Oliveira <135562519+EduardoKtzz@users.noreply.github.com>
2025-06-20 11:37:48 -0300

    Exercicio sobre listas

--- Exercicios/src/application/Program.java
+++ Exercicios/src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import domain.Numeros;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
@@ -10,13 +10,28 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite um número:");
-        int numero = sc.nextInt();
+        //Criando lista
+        List<Integer> numeros = new ArrayList<>();
 
-        Numeros numeros = new Numeros(numero);
-        String resultado = numeros.verificador(numero);
+        System.out.println("Digite 3 números:");
 
-        System.out.println(resultado);
+
+        for (int i = 0; i<3; i++) {
+            System.out.println("Número " + (i+1) + ":");
+            int numerosDigitados = sc.nextInt();
+
+            numeros.add(numerosDigitados);
+        }
+
+        int maior = numeros.get(0);
+        for (Integer numero : numeros) {
+
+            if (numero > maior) {
+                maior = numero;
+            }
+        }
+
+        System.out.println("Esse é o maior: " + maior);
 
         sc.close();
     }
